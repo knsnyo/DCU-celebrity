f15db68906694b4e8d840fb84a040ccfaf19f6da
jh Kim <39715558+knsnyo@users.noreply.github.com>
2021-11-09 17:59:39 +0900

    Add files via upload

--- 107th.ino
+++ 107th.ino
@@ -1,29 +1,32 @@
 #define YEAR 107
-int button = 14;
+#define MS 100
+
+int button = 14; // button pin 번호
 boolean button_state = false; // button 상태
-unsigned long time_previous, time_current; 
-unsigned long current_number = 0;
-bool btn_current, btn_previous = false;
+unsigned long time_previous, time_current; // 시간 확인
+unsigned long time_loop;
 
 // 0에서 9까지 숫자 표현을 위한
 // 세그먼트 a, b, c, d, e, f, g, dp의 패턴
 byte patterns[] = {
-  0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xE6
+  0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE4, 0xFE, 0xE6, 0x00
 };
 int digit_select_pin[] = {66, 67, 68, 69}; // 자릿수 선택 핀
 // 7세그먼트 모듈 연결 핀 ‘a, b, c, d, e, f, g, dp’ 순서
 int segment_pin[] = {58, 59, 60, 61, 62, 63, 64, 65};
-int count = 0;
 int SEGMENT_DELAY = 5; // 숫자 표시 사이의 시간 간격
 
-void setup(){
+bool btn_state = false; // 버튼 상태 확인
+int count = 0; // 숫자
+
+void setup() {
   for (int i = 0; i < 4; i++) { // 자릿수 선택 핀을 출력으로 설정
     pinMode(digit_select_pin[i], OUTPUT);
   }
   for (int i = 0; i < 8; i++) { // 세그먼트 제어 핀을 출력으로 설정
     pinMode(segment_pin[i], OUTPUT);
   }
-  time_previous = millis(); // 시작시간
+  //time_previous = millis(); // 시작시간
   Serial.begin(9600);
 }
 
@@ -60,34 +63,79 @@ void show_4_digit(int number) {
   delay(SEGMENT_DELAY);
 }
 
-void loop(){
-  time_current = millis(); // 현재시간
-  current_number = time_current - time_previous; // 0 ~ 9999
-  show_4_digit(current_number);
-  if(current_number > 999){
-    time_previous = time_current;
-  }
-  // 버튼 눌렸을 때
-  btn_current = digitalRead(button);
-  // Serial.print(btn_current);
-  if(btn_current){
-    Serial.print("prev : ");
-    Serial.println(time_previous);
-    Serial.print("current : ");
-    Serial.println(time_current);
-    int loop_time = time_current;    
-    if(btn_previous == false){
-      Serial.println(loop_time);
-      Serial.println("버튼 눌림");
-      btn_previous = true;      
-      while(true){
-        loop_time = millis();
-        show_4_digit(current_number);
-        if(loop_time - time_current > 3000){ break;}
-      }
+void no_show(){
+  show_digit(1, 10);
+  delay(SEGMENT_DELAY);
+  show_digit(2, 10);
+  delay(SEGMENT_DELAY);
+  show_digit(3, 10);
+  delay(SEGMENT_DELAY);
+  show_digit(4, 10);
+  delay(SEGMENT_DELAY);
+}
+
+// 숫자 반환하는 함수
+void return_count() {
+  time_previous = millis();
+  while (true) {
+    // 숫자 돌리기
+    time_current = millis();
+    if (MS <= time_current - time_previous) {
+      time_previous = time_current;
+      count += 1;
     }
-    delay(50);
-  }else{
-    btn_previous = false;
+    if (999 < count) {
+      count = 0;
+    }
+    if(digitalRead(button)){
+      count = 107;
+      break;
+    }
+    show_4_digit(count);
   }
+}
+
+// 깜빡이 만들기
+void show_result_digit(int number){
+  time_previous = millis();
+  while(true){
+    time_current = millis();
+    // 3초 반복
+    time_loop = time_current - time_previous;
+    if(3000 <= time_loop) {
+      break;
+    }
+    if(YEAR == number){
+      if(500 > time_loop){
+        show_4_digit(number);
+      }else if(1000 > time_loop){
+        no_show();
+      }else if(1500 > time_loop){
+        show_4_digit(number);
+      }else if(2000 > time_loop){
+        no_show();
+      }else if(2500 > time_loop){
+        show_4_digit(number);
+      }else{
+        no_show();
+      }
+    }else{
+      show_4_digit(number);
+    }
+  }
+}
+
+void loop() {
+  show_4_digit(count);
+  if (digitalRead(button)) {
+    // 버튼 누름 - 시작
+    delay(1000);
+    while(true){
+      return_count();
+      show_result_digit(count);
+      break;
+    }
+  }
+  btn_state = false;
+  count = 0;
 }
